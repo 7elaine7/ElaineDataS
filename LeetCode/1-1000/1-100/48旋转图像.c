65e367b0ceaa0a5db0b7cb703b0fc563e1f636e7
elsun <jiajia_test77@yeah.net>
2020-04-07 13:43:38 +0800

    48

---
+++ LeetCode/1-1000/1-100/48旋转图像.c
@@ -0,0 +1,65 @@
+/*
+48. 旋转图像
+给定一个 n × n 的二维矩阵表示一个图像。
+将图像顺时针旋转 90 度。
+说明：
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+示例 1:
+给定 matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+示例 2:
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+*/
+/*
+与面试题01.07相同
+先对角线翻折
+再左右翻折
+*/
+void rotate(int** matrix, int matrixSize, int* matrixColSize){
+    int row = matrixSize;
+    int col = *matrixColSize;
+    int m, n, tmp;
+    //对角线翻折
+    for(m = 0; m < row; ++m)
+    {
+        for(n = m; n < col; ++n)
+        {
+            tmp = matrix[m][n];
+            matrix[m][n] = matrix[n][m];
+            matrix[n][m] = tmp;
+        }
+    }
+    //左右翻折
+    for(m = 0; m < row; ++m)
+    {
+        for(n = 0; n < col / 2; ++n)
+        {
+            tmp = matrix[m][n];
+            matrix[m][n] = matrix[m][col - n - 1];
+            matrix[m][col - n - 1] = tmp;
+        }
+    }
+}
