e888d03f0f196e7292902a511fb18be887b4ad0e
elsun <jiajia_test77@yeah.net>
2020-04-19 20:07:57 +0800

    mianshi-02.04

---
+++ LeetCode/面试题/02.04分割链表.c
@@ -0,0 +1,39 @@
+/*
+面试题 02.04. 分割链表
+编写程序以 x 为基准分割链表，使得所有小于 x 的节点排在大于或等于 x 的节点之前。
+如果链表中包含 x，x 只需出现在小于 x 的元素之后(如下所示)。
+分割元素 x 只需处于“右半部分”即可，其不需要被置于左右两部分之间。
+示例:
+输入: head = 3->5->8->5->10->2->1, x = 5
+输出: 3->1->2->10->5->5->8
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* partition(struct ListNode* head, int x){
+    if(NULL == head)
+        return head;
+    struct ListNode *p = head->next;
+    struct ListNode *pre = head;
+    while(p)
+    {
+        if(p->val < x)
+        {
+            pre->next = p->next;
+            p->next = head;
+            head = p;
+            p = pre->next;
+        }
+        else
+        {
+            p = p->next;
+            pre = pre->next;
+        }
+    }    
+    return head;
+}
