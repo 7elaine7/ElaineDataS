9730e006129064334c1e70403b1c1d2d8ab606cd
elsun <jiajia_test77@yeah.net>
2020-05-03 13:42:22 +0800

    update-53

--- LeetCode/1-1000/1-100/53-最大子序和.c
+++ LeetCode/1-1000/1-100/53-最大子序和.c
@@ -9,6 +9,7 @@
 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
 */
 
+//function 1, 贪心
 int maxSubArray(int* nums, int numsSize){  
     if((NULL == nums) || (0 == numsSize))
         return INT_MIN;
@@ -25,4 +26,21 @@ int maxSubArray(int* nums, int numsSize){
             len = sum;
     }
     return len;
+}
+
+//function 2， 动态规划
+#define max(x, y) (x > y ? x : y)
+int maxSubArray(int* nums, int numsSize){  
+    int res = INT_MIN;
+    if(NULL == nums || 0 == numsSize)
+        return res;    
+    int* dp = (int*)malloc(sizeof(int) * numsSize);
+    dp[0] = nums[0];
+    res = nums[0];
+    for(int n = 1; n < numsSize; ++n)
+    {
+        dp[n] = max(dp[n - 1] + nums[n], nums[n]);
+        res = max(res, dp[n]);
+    }
+    return res;
 }
