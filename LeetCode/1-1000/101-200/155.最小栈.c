6c47ee7daa5f0954627c83f2fa2076f786356c84
elsun <jiajia_test77@yeah.net>
2020-05-12 12:39:24 +0800

    daily-155-update

--- LeetCode/1-1000/101-200/155.最小栈.c
+++ LeetCode/1-1000/101-200/155.最小栈.c
@@ -16,6 +16,7 @@ minStack.top();      --> 返回 0.
 minStack.getMin();   --> 返回 -2.
 */
 
+//function 1
 #define MAX_SIZE 20001
 typedef struct mymin{
     int min;   //top为当前节点时栈的最小值
@@ -63,6 +64,94 @@ int minStackGetMin(MinStack* obj) {
 
 void minStackFree(MinStack* obj) {
     free(obj);
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, x);
+ 
+ * minStackPop(obj);
+ 
+ * int param_3 = minStackTop(obj);
+ 
+ * int param_4 = minStackGetMin(obj);
+ 
+ * minStackFree(obj);
+*/
+
+//function 2, 栈中数据成对，奇数记录数据，偶数记录到当前元素为止最小值
+#define MAX_SIZE 20001
+typedef struct myStack{
+    int *data;
+    int top;
+} MinStack;
+
+/** initialize your data structure here. */
+
+MinStack* minStackCreate() {
+    MinStack *stack = (MinStack*)malloc(sizeof(MinStack));
+    stack->data = (int*)malloc(sizeof(int) * MAX_SIZE);
+    stack->top = -1;
+    return stack;
+}
+
+//栈里偶数位记录元素，奇数位记录到当前元素为止最小值
+void minStackPush(MinStack* obj, int x) {
+    if(obj->top == MAX_SIZE - 1)
+        return;
+    else if(-1 == obj->top)
+    {
+        //一个记录元素
+        obj->top++;
+        obj->data[obj->top] = x;
+        //一个记录最小值
+        obj->top++;
+        obj->data[obj->top] = x;
+    }
+    else
+    {
+        int tmp = obj->data[obj->top];
+        //下一个记录元素
+        obj->top++;
+        obj->data[obj->top] = x;
+        //下一个记录最小值
+        if(tmp < x)
+        {
+            obj->top++;
+            obj->data[obj->top] = tmp;
+        }
+        else
+        {
+            obj->top++;
+            obj->data[obj->top] = x;
+        }
+    }
+}
+
+void minStackPop(MinStack* obj) {
+    if(-1 == obj->top)
+        return;
+    else
+    {
+        obj->top--;
+        obj->top--;
+    }
+}
+
+int minStackTop(MinStack* obj) {
+    return obj->data[obj->top - 1];
+}
+
+int minStackGetMin(MinStack* obj) {
+    return obj->data[obj->top];
+}
+
+void minStackFree(MinStack* obj) {
+    free(obj->data);
+    obj->data = NULL;
+    free(obj);
+    obj = NULL;
 }
 
 /**
