3e55fd67a6162b5f389686615c701ec55d695860
elsun <jiajia_test77@yeah.net>
2020-09-08 12:59:04 +0800

    347 update-uthash

--- LeetCode/1-1000/301-400/347.前K个高频元素.c
+++ LeetCode/1-1000/301-400/347.前K个高频元素.c
@@ -126,3 +126,108 @@ int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
 
     return res;
 }
+
+
+//function 2， 哈希， uthash用法
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct HASH_NODE
+{
+    int key;    //哈希存储数字
+    int count;  //该数字出现的频率
+    UT_hash_handle hh;  //hash处理句柄
+}HASH;
+
+//快排回调
+int compfunc(HASH* x, HASH* y)
+{
+    return y->count - x->count;
+}
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
+    *returnSize = k;
+    HASH* head = NULL;
+    HASH* p = NULL, *s = NULL;
+    HASH* pNode = (HASH*)malloc(sizeof(HASH) * numsSize);
+
+    int n = 0;
+    for(; n < numsSize; ++n)
+    {
+        pNode[n].count = 1;
+        pNode[n].key = nums[n];
+
+        //查找哈希看该数字是否存在
+        //HASH_FIND_INT(head,&nums[i],p)      
+        //head为指向hash表的指针      
+        //&nums[i]为传入的键值    
+        //p为查找到的hash表指针
+        HASH_FIND_INT(head, &nums[n], p);
+        if(!p)
+        {
+            //如果不存在，将节点放入哈希表
+            p = &pNode[n];
+            //HASH_ADD_INT(head,key,p)        
+            //head为指向hash表的指针      
+            //key为键值       
+            //p为需要插入hash表的节点指针
+            HASH_ADD_INT(head, key, p);
+
+        }
+        else
+        {
+            p->count++;
+        }
+    }
+    n = 0;
+    //快排
+    HASH_SORT(head, compfunc);
+    //遍历
+    HASH_ITER(hh, head, p, s)
+    {
+        if(n < k)
+        {
+            nums[n++] = p->key;
+        }
+    }
+    return nums;
+}
+
+/*
+
+uthash是C的比较优秀的开源代码，它实现了常见的hash操作函数，例如查找、插入、删除等待。
+
+uthash定义，Uthash所实现的hash表中可以提供类似于双向链表的操作，可以通过结构体成员hh的 hh.prev和hh.next获取当前节点的上一个节点或者下一个节点。
+
+#include"uthash.h"
+ 
+struct my_struct {
+    int id;                     //键值 
+    char name[10];              //数据
+    UT_hash_handle hh;          //hash处理句柄，不需要为该句柄变量赋值，但必须在该结构体中定义该变量。
+};
+
+Uthash为整型key提供的查找接口为HASH_FIND_INT
+
+HASH_FIND_INT(head,&nums[i],p)      head为指向hash表的指针      &nums[i]为传入的键值    p为查找到的hash表指针
+
+Uthash为整型key提供的插入接口为HASH_ADD_INT
+
+HASH_ADD_INT(head,key,p)        head为指向hash表的指针      key为键值,结构体中成员       p为需要插入hash表的节点指针
+
+Uthash提供的删除接口为HASH_DEL
+
+HASH_DEL(head, p)      head为指向hash表的指针       p为需要删除的hash表节点指针
+
+Uthash提供的查询hash表元素个数接口为HASH_COUNT
+
+HASH_COUNT(head)     head为指向hash表的指针 
+
+Uthash提供的遍历hash表接口为HASH_ITER
+
+HASH_ITER(hh,head,p,s)      hh为hash处理句柄        head为指向hash表的指针      p为当前遍历到的结点指针     s为中间指针，用于内部操作
+
+Uthash提供的快速排序hash表接口为HASH_SORT
+
+HASH_SORT(head,compfunc)        head为指向hash表的指针      compfunc为快速排序回调函数
+
+*/
