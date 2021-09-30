09e3d3b082fa91770ddd1bc30f230bf5460aa263
elsun <jiajia_test77@yeah.net>
2021-09-30 14:34:53 +0800

    36-update

--- LeetCode/1-1000/1-100/36.有效的数独.c
+++ LeetCode/1-1000/1-100/36.有效的数独.c
@@ -112,3 +112,64 @@ bool isValidSudoku(char** board, int boardSize, int* boardColSize){
     }
     return true;
 }
+
+//function 1，简单格式优化
+#define NUM 9
+typedef struct hash_array
+{
+    int hash[10];   //0-9 hash
+}Hash_array;
+typedef struct hash_node
+{
+    Hash_array* p;
+}Hash_node;
+Hash_node* creat_hash_map(int num)
+{
+    //创建num个Hash_node型的数组，代表横，纵，3*3坐标
+    Hash_node* array = (Hash_node*)malloc(sizeof(Hash_node) * num);
+    for(int i = 0; i < num; ++i)    //0-8
+    {
+        array[i].p = (Hash_array*)malloc(sizeof(Hash_array));
+        //每个array中的数组元素下挂一个数量10的整型数组，初始化为0, (hash0, 1-9)
+        for(int c = 0; c < 10; ++c)     //0-9
+        {
+            array[i].p->hash[c] = 0;
+        }
+    }
+
+    return array;
+}
+bool isValidSudoku(char** board, int boardSize, int* boardColSize){
+    if(!board || !boardColSize || 9 != boardSize || 9 != *boardColSize)
+        return false;
+    Hash_node* X = creat_hash_map(NUM);     //row
+    Hash_node* Y = creat_hash_map(NUM);     //column
+    Hash_node* Z = creat_hash_map(NUM);     //3 * 3 区域
+
+    for(int r = 0; r < boardSize; ++r)
+    {
+        for(int c = 0; c < *boardColSize; ++c)
+        {
+            char tmp = board[r][c];
+            int tmp_i = tmp - '0';
+            if(tmp == '.')
+                continue;
+            else if(tmp >= '1' && tmp <='9')
+            {
+                int count = (r / 3) * 3 + c / 3;
+                //数字 1-9 在每一行只能出现一次 || 数字 1-9 在每一列只能出现一次 || 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次
+                if(X[r].p->hash[tmp_i] == 1 || Y[c].p->hash[tmp_i] == 1 || Z[count].p->hash[tmp_i] == 1)
+                    return false;
+                else
+                {
+                    X[r].p->hash[tmp_i] = 1;
+                    Y[c].p->hash[tmp_i] = 1;
+                    Z[count].p->hash[tmp_i] = 1;
+                }   
+            }
+            else
+                return false;
+        }
+    }
+    return true;
+}
