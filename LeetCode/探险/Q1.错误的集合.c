f85a46b1332319b557ae9a8ff6d78bfa29eeea1b
elsun <jiajia_test77@yeah.net>
2026-01-19 15:49:10 +0800

    leetcode 探险 Q1 错误的集合

---
+++ LeetCode/探险/Q1.错误的集合.c
@@ -0,0 +1,59 @@
+/*
+**************************************************************
+    Author：Elsun
+    Date：2026-1-19
+    Note: Leetcode Q1+
+**************************************************************
+Q1. 错误的集合
+集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字
+复制成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
+给定一个数组 nums 代表了该集合发生错误后的结果。
+请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+示例 1：
+输入：nums = [1,2,2,4]
+输出：[2,3]
+示例 2：
+输入：nums = [1,1]
+输出：[1,2]
+提示：
+2 <= nums.length <= 104
+1 <= nums[i] <= 104
+*/
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+    *returnSize = 2;
+    int* result = (int*)malloc((*returnSize) * sizeof(int));
+    if(!result)
+        return NULL;
+    //sort
+    for(int i = numsSize - 1; i > 0; i--)
+    {
+        bool flag = false;
+        for(int j = 0; j < i; j++)
+        {
+            if(nums[j + 1] < nums[j])
+            {
+                int tmp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = tmp;
+                flag = true;
+            }
+        }
+        if(!flag)            
+            break;
+    }
+    int sum = nums[0];
+    for(int i = 0; i < numsSize - 1; i++)
+    {
+        if(nums[i] == nums[i + 1])
+        {
+            result[0] = nums[i];
+        }
+        sum = sum + nums[i + 1];
+    }
+    result[1] = (1 + numsSize) * numsSize / 2 - (sum - result[0]);
+
+    return result;
+}
